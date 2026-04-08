4eccf4768dbdd31786f9ef31430c045a8c25565e
Eudo08 <e2khovrine92@gmail.com>
2026-04-08 08:37:06 +0200

    c

--- modules/client/MainMenu/__init__.py
+++ modules/client/MainMenu/__init__.py
@@ -26,9 +26,6 @@ class MainMenu(arcade.View):
         else:
             self.button_join.sprite = texture.get("join_default")
 
-<<<<<<< HEAD
-    @profile
-=======
 
         if self.button_create.touched:
             self.button_create.sprite = texture.get("create_hover")
@@ -36,28 +33,26 @@ class MainMenu(arcade.View):
             self.button_create.sprite = texture.get("create_default")
 
         
-
->>>>>>> 8286537e6cff0be5416583368983b13a0b2f64c0
+    
+    @profile
     def on_mouse_press(self,x,y,buttons,modifier):
         if self.button_join.touched :
             self.button_join.sprite = texture.get("join_click")
         if self.button_create.touched :
             self.button_create.sprite = texture.get("create_click")
 
+    @profile
     def on_mouse_release(self,x,y,buttons,modifier):
         if self.button_join.touched :
             self.button_join.sprite = texture.get("join_default")
 
-<<<<<<< HEAD
-    @profile
-=======
+
         if self.button_create.touched :
             self.button_create.sprite = texture.get("create_default")
 
->>>>>>> 8286537e6cff0be5416583368983b13a0b2f64c0
     def on_draw(self):
         self.clear()
-        #self.bg.draw()
+        self.bg.draw()
         self.button_join.draw()
         self.button_create.draw()
 
