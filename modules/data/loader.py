8286537e6cff0be5416583368983b13a0b2f64c0
Eudo08 <e2khovrine92@gmail.com>
2026-04-08 08:32:25 +0200

    add button create

--- modules/data/loader.py
+++ modules/data/loader.py
@@ -12,6 +12,9 @@ class Loader:
         texture.add("join_default",arcade.Sprite("assets/buttons/join_default.png"))
         texture.add("join_click",arcade.Sprite("assets/buttons/join_click.png"))
         texture.add("join_hover",arcade.Sprite("assets/buttons/join_hover.png"))
+        texture.add("create_default",arcade.Sprite("assets/buttons/create_default.png"))
+        texture.add("create_click",arcade.Sprite("assets/buttons/create_click.png"))
+        texture.add("create_hover",arcade.Sprite("assets/buttons/create_hover.png"))
 
     def load(self):
         self.load_images()
